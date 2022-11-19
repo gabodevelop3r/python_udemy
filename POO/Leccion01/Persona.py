a2bae8ce75ce739194bad901753e61994fc8244b
Gabo-dev <gabrielgallegos@MacBook-Pro-2.local>
2022-11-19 17:47:13 -0300

    [ADD] encapsulamiento en python

--- POO/Leccion01/Persona.py
+++ POO/Leccion01/Persona.py
@@ -1,23 +1,45 @@
 class Persona:
-    def __init__(self, nombre, apellido, edad, *valores, **terminos):
-        self.nombre = nombre
-        self.apellido = apellido
-        self.edad = edad
-        self.valores = valores
-        self.terminos = terminos
+    def __init__(self, nombre, apellido, edad):
+        self._nombre = nombre
+        self._apellido = apellido
+        self._edad = edad
+
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
+
+    @property
+    def apellido(self):
+        return self._apellido
+
+    @apellido.setter
+    def apellido(self, apellido):
+        self._apellido = apellido
+
+    @property
+    def edad(self):
+        return self._edad
+
+    @edad.setter
+    def edad(self, edad):
+        self._edad = edad
 
     def mostrar_detalle(self):
-        print(f'Persona: {self.nombre} {self.apellido} {self.edad} {self.valores} {self.terminos}')
+        print(f'Persona: {self._nombre} {self._apellido} {self._edad}')
+
+    def __del__(self):
+        print(f'Persona: {self._nombre} {self._apellido}')
+        del self
 
 
-print(type(Persona))
-
-
-persona1 = Persona('Juan', 'Lara', 26, '23322332', 2, 3, 4, m='manzana', p='Pera')
-print(persona1.mostrar_detalle())
-
-
-# Persona.mostrar_detalle(persona1)
-
-persona2 = Persona('Karla', 'Gomez', 30)
-persona2.mostrar_detalle()
+if __name__ == '__main__':
+    persona1 = Persona('Juan', 'Lara', 26)
+    persona1.apellido = 'Perez'
+    persona1.edad = 50
+    persona1.mostrar_detalle()
+    persona1.nombre = 'juanito'
+    print(__name__)
---
+++ POO/Leccion01/PruebaPersona.py
@@ -0,0 +1,9 @@
+from Persona import Persona
+
+print('Creacion de objetos '.center(30, '_'))
+persona1 = Persona('Carla', 'Gomez', 30)
+persona1.mostrar_detalle()
+
+
+print('Eliminacion de objetos'.center(30,'-'))
+del persona1
